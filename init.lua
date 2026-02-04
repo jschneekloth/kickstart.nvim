9f04755315d3eae6299ff1a9e6874b8c52700559
Jason Schneekloth <jschneekloth@gmail.com>
2026-02-04 01:10:15 -0600

    Fixing an error when launching

--- init.lua
+++ init.lua
@@ -614,7 +614,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
-        rust_analyzer = {},
+        -- rust_analyzer = {},
         vtsls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -632,7 +632,8 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'lua_ls', -- Lua Language server
+        --'lua_ls', -- Lua Language server
+        'lua-language-server',
         'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
       })
